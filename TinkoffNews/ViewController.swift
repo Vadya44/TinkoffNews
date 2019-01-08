2dee176ddb56356194a448913200625a9372d335
Вадим Гатауллин <Vadya@macbook-pro-vadim-2.local>
2019-01-09 03:20:46 +0500

    Initial Commit

---
+++ TinkoffNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkoffNews
+//
+//  Created by Вадим Гатауллин on 09/01/2019.
+//  Copyright © 2019 Вадим Гатауллин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
